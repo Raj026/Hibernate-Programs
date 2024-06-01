ae2edbaaa404903e720956a5b92601e013e27b8a
Raj026 <rajpatani31@gmail.com>
2024-06-01 14:33:43 +0530

    Setup the configuration File for MySQL

---
+++ ProjectWithMaven/src/main/java/com/tutorial/ProjectWithMaven/App.java
@@ -0,0 +1,25 @@
+package com.tutorial.ProjectWithMaven;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Project Started" );
+//        As the build SessionFactory is an interface we need to get it with other class Configuration
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+        
+        System.out.println(factory);
+        System.out.println(factory.isClosed());
+        
+        
+    }
+}
